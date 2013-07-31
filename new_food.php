be02e86a844e406e88e726affd5786ab95f17239
Thomas Morris <thomas.imorris@gmail.com>
2013-07-31 14:55:05 +0900

    Refactored non-POST portion of new_food.php for readability

--- new_food.php
+++ new_food.php
@@ -50,7 +50,7 @@ function display_page_header( $inTitle )
 
 
 /**
-*   create_pantry_save_form()
+*   make_pantry_save_form()
 *   =========================
 *   
 *   Creates a form in HTML that has the layout for saving a food in the user's
@@ -68,7 +68,7 @@ function display_page_header( $inTitle )
 *   @return -   $html_text  -   The html string that will build the save form
 *
 */
-function create_pantry_save_form( $default_food_name, $unit_list )
+function make_pantry_save_form( $default_food_name, $unit_list )
 {
     //TODO: make this return a string that contains all the html code instead 
     //of directly outputting it through echo.
@@ -408,11 +408,109 @@ function save_food( $save_food_vars )
     }
     echo '<p>Food Saved!</p>';
 }
+
+
+/**
+ * display_init_page()
+ * ===================
+ *
+ * Makes the initial page that prompts the user to enter a food name to search 
+ * for.  It includes a form with a text input for the user to enter a food name 
+ * along with a search button. 
+ *
+ * @param   -   null
+ *
+ * @return  -   null
+ */
+function display_init_page()
+{
+    //display the page title
+    display_page_header( $_SESSION['page_title'] );
+
+    echo '<p>Search for a food to add to your pantry</p>';
+
+    if( isset($error_array["name"]) AND $error_array["name"] == true )
+    {
+	echo '<p>Please enter a food name</p>';
+    }
+
+    echo '<form name="input" action="' . BASE_URL . 'new_food.php' . '" method="post">';
+    echo '<input type="text" name="name" value="">';
+    echo '<input type="hidden" name="status" value="name_selected">'; //since there are multiple posts on this page, this field tells the site that the first stage, the food name submission stage is complete
+    echo '<input type="submit" value="Find Food">';
+}
+
+
+/*
+ * fetch_query_results()
+ * =====================
+ *
+ * queries the ESHA database for any foods matching the passed string.  returns 
+ * the ESHA formatted object that ESHA returns.
+ *
+ * @param   -   query_str   -   the name of the food to be searched for
+ *
+ * @return  -   search_result   -   the ESHA formatted object with the query 
+*                                   results
+ */
+function fetch_query_results( $query_str )
+{
+    //get the list of foods that match the user-defined query
+    $search_result = 
+        json_decode( 
+            file_get_contents( 
+                "http://api.esha.com/foods?apikey=" .  ESHA_API_KEY . "&query=" 
+                . urlencode( $query_str ) . '&spell=true' ) 
+    ); 
+
+    $search_result = $search_result->items;
+    $_SESSION['matched_foods'] = $search_result;
+
+    return $search_result;
+}
+
+
+/*
+ * make_results_table()
+ * ====================
+ *
+ * Displays the results of an ESHA foods query in a table. Returns the html 
+ * code for the table. 
+ * 
+ * @param   -   $search_result  -   the query results from the ESHA search
+ *
+ * @return  -   $html           -   the html code for the table containing all 
+ *                                  the results
+ */
+function make_results_table( $search_result )
+{
+    $html =  '<table>';
+    $i = 0;
+
+    foreach( $search_result as $food ) {
+	$html .= '<tr>';
+	$html .= '<td>' . htmlspecialchars( $food->description ) . '</td>';
+
+	//make links that correspond to each returned food match. idx in the GET corresponds to the index of the selected food in the $_SESSION['matched_foods'] array
+	$html .= '<td><a href="' . BASE_URL . 'new_food.php?status=food_selected&idx=' . $i . '">Select</a></td>';
+	$i++;
+
+	$html .= '</tr>';
+    }
+    $html .= '</table>';
+
+    return $html;
+}
+
+
+
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%								        	%
-//%     			   POST handling				%
+//%     			   Main code    				%
 //%										%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+//POST HANDLING
 if( $_SERVER["REQUEST_METHOD"] == "POST")
 {
     $error_array = array();
@@ -456,67 +554,36 @@ if( $_SERVER["REQUEST_METHOD"] == "POST")
 } //end if request method == POST
 
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-//%								        	%
-//%     			   non-POST Stuff				%
-//%										%
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
 
-// ==================================================================
-//
+//non-POST Handling
+
+// ------------------------------------------------------------------
 // Step 1 - Searching by food name
-//
 // ------------------------------------------------------------------
 if( !isset( $_GET['status'] ) )
 {
-	//display the page title
-	display_page_header( $_SESSION['page_title'] );
-
-	echo '<p>Search for a food to add to your pantry</p>';
-
-	if( isset($error_array["name"]) AND $error_array["name"] == true )
-	{
-		echo '<p>Please enter a food name</p>';
-	}
-
-	echo '<form name="input" action="' . BASE_URL . 'new_food.php' . '" method="post">';
-	echo '<input type="text" name="name" value="">';
-	echo '<input type="hidden" name="status" value="name_selected">'; //since there are multiple posts on this page, this field tells the site that the first stage, the food name submission stage is complete
-	echo '<input type="submit" value="Find Food">';
+    display_init_page();
 }
 
-
-// ==================================================================
-//
+// ------------------------------------------------------------------
 // Step 2 - Choosing a food from the returned database possibilities
-//
 // ------------------------------------------------------------------
 else if( isset($_GET['status']) AND $_GET['status'] == "find" ) 
 {
-	//display the page title
-	display_page_header( htmlspecialchars( $_SESSION['page_title']. ' - Search Results for "' . $_SESSION['food_name_query'] ) . '"');
+    //display the page title
+    display_page_header( 
+        htmlspecialchars( 
+            $_SESSION['page_title']. ' - Search Results for "' . 
+            $_SESSION['food_name_query'] 
+        ) . '"'
+    );
 
-	//get the list of foods that match the user-defined query
-	$search_result = json_decode( file_get_contents( "http://api.esha.com/foods?apikey=" . ESHA_API_KEY . "&query=" . urlencode( $_SESSION['food_name_query'] ) . '&spell=true' ) ); 
-	$search_result = $search_result->items;
-	$_SESSION['matched_foods'] = $search_result;
-
-
-	//put a table at the bottom of the screen displaying all the results
-	echo '<table>';
-	$i = 0;
-
-	foreach( $search_result as $food ) {
-		echo '<tr>';
-		echo '<td>' . htmlspecialchars( $food->description ) . '</td>';
-
-		//make links that correspond to each returned food match. idx in the GET corresponds to the index of the selected food in the $_SESSION['matched_foods'] array
-		echo '<td><a href="' . BASE_URL . 'new_food.php?status=food_selected&idx=' . $i . '">Select</a></td>';
-		$i++;
-
-		echo '</tr>';
-	}
-	echo '</table>';
+    $search_result = fetch_query_results( $_SESSION['food_name_query'] );
+ 
+    $table_html = make_results_table( $search_result );
+    echo $table_html;
 }
 
 
@@ -527,16 +594,13 @@ else if( isset($_GET['status']) AND $_GET['status'] == "find" )
 // ------------------------------------------------------------------
 else if( isset($_GET["status"]) AND $_GET["status"] == "food_selected" )
 {
-    //TODO: use AJAX (eventually) to show nutrition facts as the user changes the serving size on this page
+    //TODO: use AJAX (eventually) to show nutrition facts as the user changes 
+    //the serving size on this page
 
-    //retrieve the selected food from the matched_foods array dependent on what idx is in the GET variable
     $_SESSION['selected_food'] = $_SESSION['matched_foods'][ $_GET['idx'] ];
     $selected_food = $_SESSION['selected_food'];
-
-    //for readability
     $food_name = $selected_food->description;
 
-    //display the page title
     display_page_header( $_SESSION['page_title'] . ' - ' . $food_name );
 
     $units = create_units_array( $_SESSION['selected_food'] );
@@ -544,90 +608,69 @@ else if( isset($_GET["status"]) AND $_GET["status"] == "food_selected" )
     //TODO: Hopefully this will look prettier with some CSS 
     //give the user the option to search for the food's nutrition facts... 
     $html_text = '<hr>';
-    $html_text .= '<h3>Search for Nutrition Facts</h3>';
-
-    $html_text .= '<form name="input" action="' . BASE_URL . 'new_food.php" method="post">';
-    $html_text .=       '<table>';
-    $html_text .=           '<tr>';
-    $html_text .=               '<th>Amount</th>';
-    $html_text .=               '<th>Units</th>';
-    $html_text .=           '</tr>';
-    $html_text .=           '<tr>';
-    $html_text .=               '<td><input type = "text" name="serving_size" value=""></td>'; //TODO: do form validation for this text input to make sure that it all numbers input the serving size input
-    $html_text .=               '<td>' . create_serving_units_dropdown( array('name' => 'serving_units'), $units ) . '</td>';
-    $html_text .=               '<td>';
-    $html_text .=                   '<input type="hidden" name="status" value="nutrition_facts"><!--//tells the site to view the nutrition facts if this is selected -->';
-    $html_text .=                   '<input type="submit" value="See Nutrition Facts">';
-    $html_text .=               '</td>';
-    $html_text .=           '</tr>';
-    $html_text .=       '</table>';
-    $html_text .= '</form>';
-    $html_text .= '<h2>OR</h2>';
-
-    //...or give them the option to save the food in the database
     $html_text .= '<h3>Save the food in your pantry</h3>';
-    $html_text .= create_pantry_save_form( $selected_food->description, $units );	
+    $html_text .= make_pantry_save_form( $selected_food->description, $units );	
 
     echo $html_text;
 }
 
 
-// ==================================================================
-//
-// Step 3.5 - Look at food nutrition facts
-//
-// ------------------------------------------------------------------
-else if( isset($_GET['status']) AND $_GET['status'] == 'nutrition_facts' )
-{
-    //require( units_table_path );
-    require_once( NUTRIENTS_TABLE_PATH );
-
-    display_page_header( "nutrition facts - " . $_session['selected_food']->description );
-    $nutrition_facts = fetch_food_details( 
-	$_session['selected_food']->id, 
-	$_session['lookup_serving_size'], 
-	$_session['lookup_serving_units_id'],  
-	esha_api_key
-    );
-
-    //show the serving size and units
-    echo '<table>';
-    echo 	'<tr>';
-    echo 		'<th>serving size:</th>';
-
-    //put the units in plural if the serving is != 1
-    //todo: implement inch'es' instead of 'inchs'.  make the units correctly either have 's' or 'es' at the end, depending on the word
-    if(	$_session['lookup_serving_size'] == 1 )
-    {
-	echo 	'<td>' . $_session['lookup_serving_size'] . ' ' . $code_to_unit_table[ $_session['lookup_serving_units_id'] ];
-    }
-    else
-    {
-	echo 	'<td>' . $_session['lookup_serving_size'] . ' ' . $code_to_unit_table[ $_session['lookup_serving_units_id'] ] . 's';
-    }
-    echo 	'</tr>';
-
-    //show each of the nutrients in it
-    foreach ($nutrition_facts as $fact) 
-    {
-	echo '<tr>';
-
-	$nutrient = $nutrients_lookup_table[ $fact->nutrient ]['description'];
-	$nutrient_unit = $nutrients_lookup_table[ $fact->nutrient ]['unit']; 
-
-	echo '<th>' . $nutrient . ':</th>';
-	echo '<td>' . $fact->value . ' ' . $nutrient_unit . '</td>';
-
-	echo '</tr>';
-    }
-    echo '</table>';
-
-    echo '<hr>';
-    echo '<p>' . htmlspecialchars('if you want to save this food in your pantry, please specify how much it costs:') . '</p>';
-
-    $units = create_units_array( $_session['selected_food'] );
-    create_pantry_save_form( $_session['selected_food']->description, $units );
-}
+// // ==================================================================
+// //
+// // Step 3.5 - Look at food nutrition facts
+// //
+// // ------------------------------------------------------------------
+// else if( isset($_GET['status']) AND $_GET['status'] == 'nutrition_facts' )
+// {
+//     //require( units_table_path );
+//     require_once( NUTRIENTS_TABLE_PATH );
+// 
+//     display_page_header( "nutrition facts - " . $_session['selected_food']->description );
+//     $nutrition_facts = fetch_food_details( 
+// 	$_session['selected_food']->id, 
+// 	$_session['lookup_serving_size'], 
+// 	$_session['lookup_serving_units_id'],  
+// 	esha_api_key
+//     );
+// 
+//     //show the serving size and units
+//     echo '<table>';
+//     echo 	'<tr>';
+//     echo 		'<th>serving size:</th>';
+// 
+//     //put the units in plural if the serving is != 1
+//     //todo: implement inch'es' instead of 'inchs'.  make the units correctly either have 's' or 'es' at the end, depending on the word
+//     if(	$_session['lookup_serving_size'] == 1 )
+//     {
+// 	echo 	'<td>' . $_session['lookup_serving_size'] . ' ' . $code_to_unit_table[ $_session['lookup_serving_units_id'] ];
+//     }
+//     else
+//     {
+// 	echo 	'<td>' . $_session['lookup_serving_size'] . ' ' . $code_to_unit_table[ $_session['lookup_serving_units_id'] ] . 's';
+//     }
+//     echo 	'</tr>';
+// 
+//     //show each of the nutrients in it
+//     foreach ($nutrition_facts as $fact) 
+//     {
+// 	echo '<tr>';
+// 
+// 	$nutrient = $nutrients_lookup_table[ $fact->nutrient ]['description'];
+// 	$nutrient_unit = $nutrients_lookup_table[ $fact->nutrient ]['unit']; 
+// 
+// 	echo '<th>' . $nutrient . ':</th>';
+// 	echo '<td>' . $fact->value . ' ' . $nutrient_unit . '</td>';
+// 
+// 	echo '</tr>';
+//     }
+//     echo '</table>';
+// 
+//     echo '<hr>';
+//     echo '<p>' . htmlspecialchars('if you want to save this food in your pantry, please specify how much it costs:') . '</p>';
+// 
+//     $units = create_units_array( $_session['selected_food'] );
+//     make_pantry_save_form( $_session['selected_food']->description, $units );
+// }
 
 
 // ==================================================================
