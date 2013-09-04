f0845f69b562bc8a03106fc94d0f73cccffc796d
Thomas Morris <thomas.imorris@gmail.com>
2013-09-04 11:12:10 +0900

    Make registration form

--- register.php
+++ register.php
@@ -5,5 +5,58 @@ require_once("/inc/config.php");
 $pageTitle = "Register";
 include( HEADER_PATH );
 
-echo 'This is Register.php! This will take care of registering new users';
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%		                                              					        	%
+//% 			                FUNCTIONS                                     %
+//%							                                                     		%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function make_registration_form()
+{
+  $form_html = "";
+  $form_html .= '<form name = "input" action="'.BASE_URL.'register.php" 
+    method="post">';
+  $form_html .= '<table>';
+
+  //Email Address
+  $form_html .= '<tr>';
+  $form_html .= '<td><th><label for="email">Email Address:</th></td>';
+  $form_html .= '<td><input type="text" name="email" id="email" 
+    size="50"></td>';
+  $form_html .= '</tr>';
+
+  //Password
+  $form_html .= '<tr>';
+  $form_html .= '<td><th><label for="password">Password:</th></td>';
+  $form_html .= '<td><input type="text" name="password" id="password" 
+    size="50"></td>';
+  $form_html .= '</tr>';
+
+  //Password Confirmation
+  $form_html .= '<tr>';
+  $form_html .= '<td><th><label for="password">Confirm 
+    Password:</th></td>';
+  $form_html .= '<td><input type="text" name="password_conf" 
+    id="password_conf" size="50"></td>';
+  $form_html .= '</tr>';
+
+  $form_html .= '</table>';
+  $form_html .= '<input type="submit" value="Register">';
+  $form_html .= '</form>';
+
+
+  return $form_html;
+}
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%		                                              					        	%
+//% 			                    MAIN CODE                                 %
+//%							                                                     		%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if( $_SERVER['REQUEST_METHOD'] == 'GET' & !isset($_GET['status']) )
+{
+  $body_html = "";
+  $body_html .= make_registration_form();
+  echo $body_html;
+}
 include( FOOTER_PATH ); 
