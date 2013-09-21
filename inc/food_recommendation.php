59be56b9921aad2d4fc38178e2eee5cb73969a5f
Thomas Morris <thomas.imorris@gmail.com>
2013-09-22 07:58:26 +0900

    Fix indentation in inc/food_recommendation.php

--- inc/food_recommendation.php
+++ inc/food_recommendation.php
@@ -1,9 +1,9 @@
 <?php
 /*
-*	This page is responsible for taking in a get request containing a 
-*	food name, searching for any matching foods that are saved in the
-*	user's pantry, and returning those results.
-*/
+ *	This page is responsible for taking in a get request containing a 
+ *	food name, searching for any matching foods that are saved in the
+ *	user's pantry, and returning those results.
+ */
 
 session_start();
 if( $_GET['user_input'] )
@@ -11,22 +11,22 @@ if( $_GET['user_input'] )
 	$user_input = $_GET['user_input'];
 	$saved_foods = $_SESSION['saved_foods'];
 
-        //$matched_results is the list of saved foods that match the name typed 
-        //by the user
+  //$matched_results is the list of saved foods that match the name typed 
+  //by the user
 	$matched_results = array(); 
-	
+
 
 	foreach( $saved_foods as $saved_food )
 	{
-	    if (stristr( $saved_food['user_def_food_name'], $user_input ) )
-	    {
-		$matched_results[] = array( 
+	  if (stristr( $saved_food['user_def_food_name'], $user_input ) )
+	  {
+		  $matched_results[] = array( 
 		    'category'  => 'Your Saved Foods:',
 		    'label'     => $saved_food['user_def_food_name'],
 		    'food_id'   => $saved_food['id']
 		    //'units' => /*TODO: Add units based on what food it is*/
-		);
-	    }
+		  );
+	  }
 	}
 
 	echo json_encode( $matched_results );
