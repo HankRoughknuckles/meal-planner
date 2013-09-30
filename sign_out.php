a6748bcee1c8d1a0bb3eb79fdea0a6befe9bca8c
Thomas Morris <thomas.imorris@gmail.com>
2013-09-30 16:05:10 +0900

    Finish sign_out.php

--- sign_out.php
+++ sign_out.php
@@ -11,19 +11,22 @@ include( HEADER_PATH );
 //% 			                    MAIN CODE
 //%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if( $_SERVER['REQUEST_METHOD'] == 'GET' )
+if( !isset($_GET['status']) )
 {
-  
   $body_html = '';
+  $body_html .= '<p>Are you sure you want to log out?</p>';
+  $body_html .= '<a href="'.SIGN_OUT_PATH.'?status=signed-out">Sign 
+    out for real!</a><br />';
+  $body_html .= '<a href="'.BASE_URL.'">Go back</a>';
+  echo $body_html;
+}
 
-  if( $_SESSION['user_id'] !=  NOT_LOGGED_IN )
-  {
-    $body_html .= '<p>Are you sure you want to log out?</p>';
-    $body_html .= '<a href="'.SIGN_OUT_PATH.'?status=signed-out">Sign out 
-      for real!</a><br />';
-    $body_html .= '<a href="'.BASE_URL.'">Go back</a>';
-    echo $body_html;
-  }
+
+else
+{
+  $_SESSION['user_id'] = NOT_LOGGED_IN;
+  echo '<pre>'; var_dump($_SESSION['user_id']); echo '</pre>'; die();
+  header( "Location: " . BASE_URL . "index.php" );
 }
 
 include( FOOTER_PATH ); 
