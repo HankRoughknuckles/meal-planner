00cddb9ae08a16bfd48778ffb2ccf667ac993ae7
Thomas Morris <thomas.imorris@gmail.com>
2013-10-01 18:33:17 +0900

    Increase Modularity   -(NOTE: COMMIT IS BROKEN)   -Make functions:     -make_label()     -make_html_tag()

--- register.php
+++ register.php
@@ -29,6 +29,8 @@ function make_registration_form( $errors = null, $old_input = null )
 structure -->
     <table>
 
+<!-- TODO: the html formatting on this table is messed up.  Look at the 
+html source from the webpage. -->
     <?php 
     $needles = array('email_syntax', 'email_uniqueness');
     make_text_input('email', "Email Address", $old_input['email'], 
@@ -52,9 +54,9 @@ structure -->
 /**
  * make_text_input()
  * ================
- * outputs the html for a text input.  Includes ability to display if the 
- * input has an error that needs to be corrected by the user.  The input 
- * will have the falue passed in $old_input
+ * Makes a text input with label for a form.  Includes ability to display 
+ * if the input has an error that needs to be corrected by the user.  The 
+ * input will have the falue passed in $old_input
  *
  * @param - $name       - the variable name that the form will post to
  * @param - $label      - the label for the input
@@ -74,9 +76,10 @@ function make_text_input($name, $label, $value, $needles, $errors)
     <tr>
       <td>
         <th>
-          <label class="control-label" for="<?php echo $name;?>">
-            <?php echo $label;?>
-          </label>
+          <?php make_label( $label, array( 
+            "for" => $name,
+            "class" => "control-label") 
+          ); ?>
         </th>
       </td>
       <td>
@@ -93,6 +96,20 @@ function make_text_input($name, $label, $value, $needles, $errors)
   </div>
 <?php }
 
+/**
+ * make_password_input()
+ * =====================
+ *
+ * creates an label and an input for a password, 
+ *
+ * @param - $name       - the variable name that the form will post to
+ * @param - $label      - the label for the input
+ * @param - $value      - the value that the input will display (useful if 
+ *                        the user previously information incorrectly and 
+ *                        you want to restore the old input they had 
+ *                        before)
+ * @param - $has_error  - if == TRUE - input will have red outline
+ */
 function make_password_input($name, $label, $value, $needles, $errors)
 {?>
   <?php if( is_error_present($needles, $errors) ) { ?>
@@ -103,9 +120,10 @@ function make_password_input($name, $label, $value, $needles, $errors)
     <tr>
       <td>
         <th>
-          <label class="control-label" for="<?php echo $name;?>">
-            <?php echo $label;?>
-          </label>
+          <?php make_label( $label, array( 
+            "for" => $name,
+            "class" => "control-label") 
+          ); ?>
         </th>
       </td>
       <td>
@@ -121,31 +139,72 @@ function make_password_input($name, $label, $value, $needles, $errors)
     </tr>
   </div>
 <?php }
+
+
+/**
+ * make_label()
+ */
+function make_label( $value, $options )
+{
+  $options['tag'] = 'label';
+  $required_options = array('for');
+  $tag = make_html_tag( $options, $required_options );
+  $tag .= $value;
+  $tag .= make_html_tag( array( "tag" => "/label"), array() );
+  echo $tag;
+}
+
+
 /**
  * make_base_input()
- *
+ * =================
  * makes an input with the types specified in the input $options 
  * associative array
  */
 function make_base_input( $options )
 {
-  if( !isset($options['type']) OR !isset($options['name']) )
+  $required_options = array( 'type', 'name' );
+  $options['tag'] = 'input';
+  echo  make_html_tag( $options, $required_options );
+}
+
+
+/**
+ * make_html_tag()
+ * ===============
+ * makes an html tag with the passed options.  Will return FALSE if any 
+ * options with the names present in $required_options are not present.  
+ * Will also return FALSE if $options does not have a 'tag' entry.
+ */
+function make_html_tag( $options, $required_options = array() )
+{
+  //send error if required options are not present
+  foreach( $required_options as $req_option )
+  {
+    if( !isset($options[$req_option]) )
+    {
+      return "FALSE";
+    }
+  }
+  if( !isset($options['tag']) )
   {
     return "FALSE";
   }
 
-  $input = "<input ";
+  //build the tag
+  $tag = '<'.$options['tag'];
+  unset($options['tag']);
   foreach( $options as $key => $option )
   {
     if( $option )
     {
-      $input .= "$key=\"$option\" ";
+      $tag .= " $key=\"$option\"";
     }
   }
 
-  $input .= '>';
+  $tag .= '>';
 
-  echo $input;
+  return $tag;
 }
 
 /**
