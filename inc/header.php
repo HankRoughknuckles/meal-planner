f1108c0cb4d6fa01c2b5b9ef6baa10b8c6e24b46
Thomas Morris <thomas.imorris@gmail.com>
2013-10-04 11:01:56 +0900

    Add more to header navbar   -Display differently if signed in or not

--- inc/header.php
+++ inc/header.php
@@ -1,14 +1,13 @@
-<html>
-<head>
-	<?php
-	require_once("/inc/config.php");
-	require_once( KEYS_PATH );
+<?php
+require_once("/inc/config.php");
+require_once( KEYS_PATH );
+session_start();
 
-	//set the page title
-	if( $pageTitle == "Index" ) 
-	{
-		$pageTitle = "Home";
-	}
+//set the page title
+if( $pageTitle == "Index" ) 
+{
+	$pageTitle = "Home";
+}
 
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -22,12 +21,16 @@ function makeNavbar()
 ?>
 <div class="navbar navbar-fixed-top">
   <div class="navbar-inner">
-    <div class="container">
-      <ul class="nav">  
+    <div class="container-fluid">
         <a class="brand" href="#">Meal Planner</a>  
-        <li><a href="#">About</a></li>  
-        <li><a href="#">Portfolio</a></li>  
-        <li><a href="#">Contact</a></li>  
+      <ul class="nav pull-right">  
+      <?php if ($_SESSION['user_id'] == NOT_LOGGED_IN){ ?>
+        <li><a href="#">*THE_USERNAME_HERE*</a></li>  
+        <li><a href="#">Sign out</a></li>  
+      <?php } else { ?>
+      <li><a href="<?php echo REGISTER_PATH;?>">Register</a></li>  
+        <li><a href="#">Log in</a></li>  
+      <?php } ?>
       </ul>
     </div>
     <!-- <a class="navbar_brand" href="#">Meal Planner</a> -->
@@ -44,38 +47,42 @@ function makeNavbar()
 //%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ?>
-	<title>Meal Planner - "<?php echo $pageTitle; ?>"</title>
+<html>
+<head>
+  <title>
+    Meal Planner - "<?php echo $pageTitle; ?>"
+  </title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<link rel="stylesheet" type="text/css" href="<?php echo STYLE_PATH; ?>" />
-	<link rel="stylesheet" href="<?php echo JQUERY_UI_STYLE_PATH; ?>" />
+  <link rel="stylesheet" type="text/css" href="<?php echo STYLE_PATH; ?>">
+  <link rel="stylesheet" href="<?php echo JQUERY_UI_STYLE_PATH; ?>">
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-	<script src="<?php echo JQUERY_UI_PATH; ?>"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+  <script src="<?php echo JQUERY_UI_PATH; ?>"></script>
   <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-  
-  <div class="container-fluid">
-  <?php makeNavbar(); ?>
+
+<div class="container-fluid">
+<?php makeNavbar(); ?>
+  <!-- TODO: make a homepage for when user is logged out, and a user profile page when logged in -->
+	<h2><?php echo $pageTitle ?></h2>
+  <div class="row-fluid">
+    <div class="span2">
+      <ul>
+        <li>
+          <a href="<?php echo BASE_URL ?>new_recipe.php">New Recipe</a>
+        </li>
+        <li>
+          <a href="<?php echo BASE_URL ?>new_food.php">New Food</a>
+        </li>
+        <li>
+          <a href="<?php echo BASE_URL ?>view_foods.php">My Foods</a>
+        </li>
+        <li>
+          <a href="<?php echo BASE_URL ?>view_recipes.php">My Recipes</a>
+        </li>
+      </ul>
+    </div>
+    <div class="span10">
 	  <h1><a href="<?php echo BASE_URL; ?>">Meal Planner</a></h1>
-    <!-- TODO: make a homepage for when user is logged out, and a user profile page when logged in -->
-	  <h2><?php echo $pageTitle ?></h2>
-    <div class="row-fluid">
-      <div class="span2">
-        <ul>
-          <li>
-            <a href="<?php echo BASE_URL ?>new_recipe.php">New Recipe</a>
-          </li>
-          <li>
-            <a href="<?php echo BASE_URL ?>new_food.php">New Food</a>
-          </li>
-          <li>
-            <a href="<?php echo BASE_URL ?>view_foods.php">My Foods</a>
-          </li>
-          <li>
-            <a href="<?php echo BASE_URL ?>view_recipes.php">My Recipes</a>
-          </li>
-        </ul>
-      </div>
-      <div class="span10">
