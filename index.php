9c90f543d0360a1e8be44d94488157a642793031
Thomas Morris <thomas.imorris@gmail.com>
2013-10-04 13:25:18 +0900

    Modify index.php to eliminate redundancy

--- index.php
+++ index.php
@@ -5,21 +5,10 @@ require_once HELPERS_PATH;
 display_page_header("Meal Planner");
 
 if( $_SESSION['user_id'] == NOT_LOGGED_IN ){ ?>
-  <div class='login'>
-    <ul>
-      <li><a href="<?php echo BASE_URL ?>sign_in.php">Sign In</a></li>
-      <li><a href="<?php echo BASE_URL ?>register.php">Register</a></li>
-    </ul>
-  </div> <!-- END login div -->
-
+  <p>Make welcome page talking about all the stuff the meal planner can 
+  do.</p>
 <?php } else { ?>
-
-  <div class='login'>
-    <ul>
-      <li><a href="<?php echo BASE_URL ?>sign_out.php">Sign Out</a></li>
-    </ul>
-  </div> <!-- END login div -->
-
+  <p>Display the user's profile page here.</p>
 <?php } ?>
 
 <?php include( FOOTER_PATH ); 
